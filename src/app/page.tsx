409a34c9421f27c2aa8d074aec1ad4d7bcc07d5c
Kimberly Wilkes <kimowilkes@gmail.com>
2024-05-21 13:16:30 -0400

    chore: add and update linter and formatter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import { Box, Typography } from "@mui/material";
-import Image from "next/image";
+import { Box, Typography } from '@mui/material'
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -9,5 +9,5 @@ export default function Home() {
         Testing material-ui box and typography
       </Box>
     </main>
-  );
+  )
 }
